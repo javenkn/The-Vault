902ed769017b280cf34390567dd6736727396195
javenkn <javenkn7@yahoo.com>
2016-05-17 00:32:13 -1000

    passed all tests

--- vault.js
+++ vault.js
@@ -1,4 +1,18 @@
 'use strict';
 module.exports = function() {
-  
+  var vaultValue = null;
+  return {
+    //setValue function sets the value in the vault
+    setValue: function(key, value){
+      vaultValue = value;
+    },
+    //getValue function returns value if there is a key
+    //returns null if a key doesn't exist
+    getValue: function(key){
+      if(key !== null)
+        return vaultValue;
+      else
+        return null;
+    }
+  };
 };
